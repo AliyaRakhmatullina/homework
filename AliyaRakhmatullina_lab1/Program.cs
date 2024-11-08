7072ea7cc30b7510f8572ca0dc41a1d84d421dcf
AliyaRakhmatullina <9874208544al@gmail.com>
2024-11-08 09:03:01 +0300

    Добавьте файлы проекта.

---
+++ AliyaRakhmatullina_lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliyaRakhmatullina_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.Вывести на экран число e с точностью до десятых.
+            Console.WriteLine("1 задание");
+
+            double e = Math.E;// Math.E - константа из класса Math!
+            Console.WriteLine($"Число e:{e:F1}");
+
+                        
+
+            
+
+
+        }
+    }
+}
